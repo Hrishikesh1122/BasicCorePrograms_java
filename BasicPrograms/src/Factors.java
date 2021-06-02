31587e294f91da4c1b25b4e4d9a4546bf8eef8ff
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-06-02 15:34:24 +0530

    Added:factors problem

---
+++ BasicPrograms/src/Factors.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class Factors {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number to compute factors:");
+		int num=sc.nextInt();
+		while (num%2==0)
+		{
+			System.out.println(2);
+			num/=2;
+		}
+		
+		for (int i=3;i<=Math.sqrt(num);i+=2)
+		{
+			while(num%i == 0)
+			{
+				System.out.println(i);
+				num/=i;
+			}
+		}
+		
+		if(num>2)
+			System.out.println(num);
+
+        sc.close();
+	}
+
+}
